97d39775313e8a698d43dae8168537284cb3a4f5
alicanyayl <alicanuyelikhesap@gmail.com>
2026-08-10 11:20:20 +0300

    fix: add ts-expect-error for netinfo-mock import

--- test-utils/setupTests.ts
+++ test-utils/setupTests.ts
@@ -1,3 +1,4 @@
+// @ts-expect-error netinfo-mock.js does not carry declaration file
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
